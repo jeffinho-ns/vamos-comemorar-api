f51ef4823e8c78442c512f4048132f894c6b6f96
Jefferson <jeffinho_ns@hotmail.com>
2026-01-13 13:02:54 -0300

    fix: adiciona mÃ©todo PATCH aos mÃ©todos permitidos no CORS

--- config/environment.js
+++ config/environment.js
@@ -17,7 +17,7 @@ module.exports = {
         'https://www.grupoideiaum.com.br'
       ],
       credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Content-Length']
     }
   },
